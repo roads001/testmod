5899d0777072a24ba7ebbc0a074cafcbf63a52d0
james <james@pemlex.de>
2023-03-31 08:54:30 +0200

    add .gitignore

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,5 +1,12 @@
 package helloworld
 
+import "fmt"
+
 func Hello() string {
 	return "Hello, world."
+}
+
+func Hi(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
